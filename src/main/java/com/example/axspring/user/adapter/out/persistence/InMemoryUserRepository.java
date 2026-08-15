9e1ee2edcb05de17b8b13568b1cfbe3117596fa8
Lee-Dongwook <dlehddnr0713@gmail.com>
2026-08-15 14:16:58 +0900

    fix: profile in-memory seperate

--- src/main/java/com/example/axspring/user/adapter/out/persistence/InMemoryUserRepository.java
+++ src/main/java/com/example/axspring/user/adapter/out/persistence/InMemoryUserRepository.java
@@ -3,6 +3,7 @@ package com.example.axspring.user.adapter.out.persistence;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import com.example.axspring.user.application.port.out.UserRepository;
@@ -14,6 +15,7 @@ import com.example.axspring.user.domain.User;
  * 애플리케이션을 재시작하면 저장된 데이터는 초기화됩니다.
  */
 @Repository
+@Profile("in-memory")
 public class InMemoryUserRepository implements UserRepository {
 
     private final Map<String, User> usersByEmail = new ConcurrentHashMap<>();
